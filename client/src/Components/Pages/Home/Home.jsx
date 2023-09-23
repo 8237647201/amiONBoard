c4ba20010eb60f2b21bd38ad11dd31a9a8669b21
Ankit Mishra <ankitmishra823764@gmail.com>
2023-09-23 22:38:57 +0530

    Changes in home page

--- client/src/Components/Pages/Home/Home.jsx
+++ client/src/Components/Pages/Home/Home.jsx
@@ -205,97 +205,232 @@
 //     </div>
 //   );
 // }
-import React, { useState } from "react"; // Import React and useState
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+// import React, { useState } from "react"; // Import React and useState
+// import { Link } from "react-router-dom"; // Import Link from react-router-dom
+// import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+// import BackgroundImage from "../../Image/stockimage.jpeg";
+// import Service from "../Service/Service";
+// import SavingsIcon from "@mui/icons-material/Savings";
+// import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
+// import PeopleIcon from "@mui/icons-material/People";
+
+// const iconStyle = {
+//   fontSize: "50px",
+//   color: "#ff9000",
+//   alignItems: "center",
+//   width: "80%",
+// };
+
+// const containerStyle = {
+//   backgroundColor: "#f7d9ce",
+//   minHeight: "100vh",
+//   display: "flex",
+//   flexDirection: "column",
+//   alignItems: "top",
+//   justifyContent: "center",
+// };
+
+// const ButtonStyle = {
+//   position: "relative",
+//   bottom: "650px",
+//   backgroundColor: "orange",
+//   color: "black",
+//   marginTop: "20px",
+//   marginLeft: "43%",
+//   height: "45px",
+//   width: "220px",
+// };
+
+// const textStyle = {
+//   position: "relative",
+//   bottom: "300px",
+//   textAlign: "center",
+// };
+
+// const image_container = {
+//   height: "600px",
+//   overflow: "hidden",
+//   objectFit: "cover",
+// };
+
+// const buttonHoverStyle = {
+//   backgroundColor: "#7a3517", 
+//   color: "red",
+// };
+
+// export default function Home() {
+//   const [isHovered, setIsHovered] = useState(false); // Initialize isHovered state
+
+//   const handleMouseEnter = () => {
+//     setIsHovered(true);
+//   };
+
+//   const handleMouseLeave = () => {
+//     setIsHovered(false);
+//   };
+
+//   return (
+//     <>
+//       <div style={image_container}>
+//         <img src={BackgroundImage} style={{ width: "100%" }} alt="Background" />
+//       </div>
+//       <div style={containerStyle}>
+//         <h1 style={textStyle}>
+//           <span style={{ color: "#7a3517", fontSize: "35px" }}>Book your</span>
+//           <span style={{ color: "#ed530e", fontSize: "35px" }}> own ride! </span>
+//         </h1>
+//       </div>
+//       <div style={{ width: "100%" }}>
+//         <Link to="/ride">
+//           <button
+//             style={{
+//               ...ButtonStyle,
+//               ...(isHovered && { ...buttonHoverStyle }), // Apply hover styles
+//             }}
+//             onMouseEnter={handleMouseEnter}
+//             onMouseLeave={handleMouseLeave}
+//           >
+//             Check for Ride
+//           </button>
+//         </Link>
+//       </div>
+//       <Service />
+//     </>
+//   );
+// }
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
 import BackgroundImage from "../../Image/stockimage.jpeg";
-import Service from "../Service/Service";
-import SavingsIcon from "@mui/icons-material/Savings";
-import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
-import PeopleIcon from "@mui/icons-material/People";
+import Bg1 from "../../Image/Home.jpg";
+import{ useState, useEffect } from 'react';
+import { height, maxHeight } from '@mui/system';
+import SavingsIcon from '@mui/icons-material/Savings';
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
+import PeopleIcon from '@mui/icons-material/People';
+import Service from '../Service/Service';
+import { Link } from 'react-router-dom';
 
 const iconStyle = {
-  fontSize: "50px",
-  color: "#ff9000",
-  alignItems: "center",
-  width: "80%",
+  fontSize: '50px', 
+  color: '#ff9000',
+  alignItems:'center',
+  width:'80%'   
 };
+const divstyle={
+  width:'1000px',
+  color:'#0d264c',
+  display:'flex',
+  justifyContent:'space-between',
 
+}
 const containerStyle = {
-  backgroundColor: "white",
-  minHeight: "100vh",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "top",
-  justifyContent: "center",
+  maxHeight: '8vh',
+  display: 'flex',
+  flexDirection: 'column',
+  marginBottom:'0px',
+  padding:'0px',
+  alignItems: 'top',
+  justifyContent: 'center',
 };
-
-const ButtonStyle = {
-  position: "relative",
-  bottom: "650px",
-  backgroundColor: "orange",
-  color: "black",
-  marginTop: "40px",
-  marginLeft: "43%",
-  height: "45px",
-  width: "220px",
-};
+const ButtonStyle={
+  marginBottom:'0px',
+  backgroundColor:'white',
+  color: '#ff9000',
+  marginTop:'90px',
+  marginLeft:'43%',
+  height:'45px',
+  width:'230px',
+  transition: 'background-color 0.3s, transform 0.3s', // Add transition for smooth animation
+  transformOrigin: 'center',
+  hoverButtonStyle:"transform: 'scale(2.0)'", 
+}
 
 const textStyle = {
-  position: "relative",
-  bottom: "300px",
-  textAlign: "center",
+marginTop:'100px',
+ marginBottom:'20px',
+  textAlign: 'center', 
+  animation: 'moveRight 3s infinite',
 };
-
-const image_container = {
-  height: "600px",
-  overflow: "hidden",
-  objectFit: "cover",
+const image_container= {
+  height:'550px',
+  overflow:'hidden',
+  object_fit:'cover',
+}
+const animatedTextStyle = {
+  color:'black',
+  animation: 'moveRight 2s infinite', // Animation name, duration, and repetition
 };
+export default function BasicSelect() {
+  const [selectedFrom, setSelectedFrom] = React.useState('');
+  const [selectedTo, setSelectedTo] = useState('');
 
-const buttonHoverStyle = {
-  backgroundColor: "#7a3517", 
-  color: "red",
-};
-
-export default function Home() {
-  const [isHovered, setIsHovered] = useState(false); // Initialize isHovered state
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
+  const [inputValue, setInputValue] = useState('');
+  const handleChange = (event) => {
+    setSelectedFrom(event.target.value);
+  };
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleChangeTo = (event) => {
+    setSelectedTo(event.target.value);
   };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
 
   return (
-    <>
-      <div style={image_container}>
-        <img src={BackgroundImage} style={{ width: "100%" }} alt="Background" />
-      </div>
-      <div style={containerStyle}>
-        <h1 style={textStyle}>
-          <span style={{ color: "#7a3517", fontSize: "35px" }}>Book your</span>
-          <span style={{ color: "#ed530e", fontSize: "35px" }}> own ride! </span>
-        </h1>
-      </div>
-      <div style={{ width: "100%" }}>
-        <Link to="/ride">
-          <button
-            style={{
-              ...ButtonStyle,
-              ...(isHovered && { ...buttonHoverStyle }), // Apply hover styles
-            }}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            Check for Ride
-          </button>
-        </Link>
-      </div>
-      <Service />
-    </>
+    <><div>
+      <style>
+       {`
+        @keyframes moveRight {
+          0% {
+            transform: translateX(0);
+          }
+          50% {
+            transform: translateX(50px);
+          }
+          100% {
+            transform: translateX(0);
+          }
+        }
+      `}
+      </style>
+    </div><>
+    <div style={image_container}>
+      <img src={BackgroundImage} style={{ width: '100%' }} />
+    </div>
+        <div style={containerStyle}>
+          <h1 style={textStyle}>
+            <span style={{ color: '#003366', fontSize: '35px' }}>Book your</span>
+            <span style={{ color: '#FFA500', fontSize: '35px' }}> own ride
+            </span>
+          </h1>
+        </div>
+        <div style={{ width: '100%' }}>
+    <Link to="/Ride">
+    <Button variant="contained"
+      style={ButtonStyle}
+      onMouseEnter={(e) => {
+        e.target.style.backgroundColor = 'purple';
+        e.target.style.transform = 'scale(1.1)';
+      }}
+      onMouseLeave={(e) => {
+        e.target.style.backgroundColor = 'white';
+        e.target.style.transform = 'scale(1)';
+      }}>
+      Check for Ride
+    </Button>
+  </Link>
+</div>
+
+
+        <Service style={{ marginTop: '0' }}/>
+        
+
+      </></>
   );
 }
--- client/src/Components/Pages/Service/Service.jsx
+++ client/src/Components/Pages/Service/Service.jsx
@@ -1,20 +1,115 @@
+// import React from "react";
+// import SavingsIcon from "@mui/icons-material/Savings";
+// import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
+// import PeopleIcon from "@mui/icons-material/People";
+
+// const containerStyle = {
+//   backgroundColor: "#f3cebf",
+//   minHeight: "100vh",
+//   display: "flex",
+//   flexDirection: "column",
+//   alignItems: "center",
+//   justifyContent: "center",
+//   marginTop: "20px",
+//   position: "relative", // Added
+//   bottom: "600px", // Added
+// };
+
+// const iconStyle = {
+//   fontSize: "50px",
+//   color: "#ff9000",
+//   alignItems: "center",
+//   width: "80%",
+// };
+
+// const textStyle = {
+//   textAlign: "center", // Center text within its container
+// };
+
+// const divstyle = {
+//   width: "1000px",
+//   color: "#0d264c",
+//   display: "flex",
+//   justifyContent: "space-between",
+// };
+
+// const Service = () => {
+//   return (
+//     <div style={containerStyle}>
+//       <div style={{ position: "relative", bottom: "30px", right: "10px" }}>
+//         <h1 style={textStyle}>
+//           <span style={{ color: "black", fontSize: "50px" }}>What's in it</span>
+//           <span style={{ color: "#ed530e", fontSize: "50px" }}> for you </span>
+//         </h1>
+//       </div>
+//       <div style={divstyle}>
+//         <div
+//           style={{
+//             width: "250px",
+//             marginRight: "60px",
+//             alignItems: "center",
+//             marginLeft: "75px",
+//           }}
+//         >
+//           <div>
+//             {" "}
+//             <SavingsIcon style={iconStyle} />
+//           </div>
+//           <h1>AFFORDABILITY</h1>
+//           <p style={{ color: "black", fontSize: "20px" }}>
+//             No more latent prices and shocked reactions after seeing the final
+//             ride cost.
+//             <br />
+//             We are here with the most affordable rides for all.
+//           </p>
+//         </div>
+//         <div
+//           style={{ width: "300px", marginRight: "60px", marginLeft: "60px" }}
+//         >
+//           <div>
+//             {" "}
+//             <TwoWheelerIcon style={iconStyle} />
+//           </div>
+
+//           <h1>QUICK RIDE</h1>
+//           <p style={{ color: "black", fontSize: "20px" }}>
+//             Trend setters of smart commute be it traffic or rush hours.
+//           </p>
+//         </div>
+//         <div
+//           style={{ width: "300px", marginRight: "60px", marginLeft: "60px" }}
+//         >
+//           <div>
+//             {" "}
+//             <PeopleIcon style={iconStyle} />
+//           </div>
+
+//           <h1>MAKE FRIENDS</h1>
+//           <p style={{ color: "black", fontSize: "20px" }}>
+//             Get to know unique people from your university and opportunity to
+//             socialize more and create useful contacts.
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Service;
 import React from "react";
 import SavingsIcon from "@mui/icons-material/Savings";
 import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
 import PeopleIcon from "@mui/icons-material/People";
-
 const containerStyle = {
+  
   backgroundColor: "white",
   minHeight: "100vh",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  marginTop: "20px",
-  position: "relative", // Added
-  bottom: "600px", // Added
-};
-
+  
+};  
 const iconStyle = {
   fontSize: "50px",
   color: "#ff9000",
@@ -23,23 +118,26 @@ const iconStyle = {
 };
 
 const textStyle = {
+  marginTop:'20%',
   textAlign: "center", // Center text within its container
 };
-
 const divstyle = {
+  position:'relative',
+  bottom:'80px',          
   width: "1000px",
   color: "#0d264c",
   display: "flex",
   justifyContent: "space-between",
 };
-
 const Service = () => {
   return (
     <div style={containerStyle}>
-      <div style={{ position: "relative", bottom: "30px", right: "10px" }}>
+      <div style={{ position: "relative", bottom: "150px", right: "10px" }}>
         <h1 style={textStyle}>
-          <span style={{ color: "black", fontSize: "50px" }}>What's in it</span>
-          <span style={{ color: "#ed530e", fontSize: "50px" }}> for you </span>
+          <span style={{ color: '#003366', fontSize: "35px" }}>
+            What's in it{" "}
+          </span>
+          <span style={{ color: "#ff9000", fontSize: "35px" }}> for you </span>
         </h1>
       </div>
       <div style={divstyle}>
@@ -55,8 +153,8 @@ const Service = () => {
             {" "}
             <SavingsIcon style={iconStyle} />
           </div>
-          <h1>AFFORDABILITY</h1>
-          <p style={{ color: "black", fontSize: "20px" }}>
+          <h1 style={{textAlign:'center',fontSize:'25px'}}>AFFORDABILITY</h1>
+          <p style={{ color: "black", fontSize: "18px" }}>
             No more latent prices and shocked reactions after seeing the final
             ride cost.
             <br />
@@ -71,8 +169,8 @@ const Service = () => {
             <TwoWheelerIcon style={iconStyle} />
           </div>
 
-          <h1>QUICK RIDE</h1>
-          <p style={{ color: "black", fontSize: "20px" }}>
+          <h1 style={{textAlign:'center',fontSize:'25px'}}>QUICK RIDE</h1>
+          <p style={{ color: "black", fontSize: "18px" }}>
             Trend setters of smart commute be it traffic or rush hours.
           </p>
         </div>
@@ -84,10 +182,10 @@ const Service = () => {
             <PeopleIcon style={iconStyle} />
           </div>
 
-          <h1>MAKE FRIENDS</h1>
-          <p style={{ color: "black", fontSize: "20px" }}>
-            Get to know unique people from your university and opportunity to
-            socialize more and create useful contacts.
+          <h1 style={{textAlign:'center',fontSize:'25px'}}>MAKE FRIENDS</h1>
+          <p style={{ color: "black", fontSize: "18px" }}>
+            Get to know unique people from your university and oppurtunity to
+            socialize more and create useful contacts
           </p>
         </div>
       </div>
