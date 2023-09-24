c82402ac914ba854c4f46e518be7b4bd157ab42f
Ankit Mishra <ankitmishra823764@gmail.com>
2023-09-24 15:46:19 +0530

    request chages

--- client/src/Components/Pages/Card/RequestCard.jsx
+++ client/src/Components/Pages/Card/RequestCard.jsx
@@ -36,18 +36,98 @@
 
 
 // export default RequestCard;
+// import React from "react";
+// import {
+//   Card,
+//   CardContent,
+//   Typography,
+//   IconButton,
+//   Box,
+//   styled,
+// } from "@mui/material";
+// import DeleteIcon from "@mui/icons-material/Delete";
+// import DoneIcon from '@mui/icons-material/Done';
+// import {deletUserBooking} from "../../API/fetchApi.js"
+
+// const RequestBox = styled(Box)`
+//   border: 1px solid #000;
+//   padding: 20px;
+//   margin-top: 20px;
+//   width: 90%;
+//   text-align: center;
+//   background-color: #fff; 
+// `;
+
+// const RequestCard = ({ form, viewMode }) => {
+
+//     //deleting the request
+
+//     const onDelete = async(username)=>{
+//          const res = await deletUserBooking(username)
+        
+//     }
+//    const onAccept = ()=>{}
+
+
+
+
+//   if (viewMode === "compact") {
+//     return (
+//       <Card style={{ marginBottom: "10px" }}>
+//         <CardContent>
+//           <Typography variant="body1" style={{ display: "inline-block" }}>
+//             {form.from} --To--  {form.to}  -- Created By -- {form.username}
+//           </Typography>
+//           <IconButton
+//             aria-label="delete"
+//             style={{ float: "right" }}
+//             onClick={() => onAccept(form.id)} // You should pass a unique identifier like form.id for each booking
+//           >
+//             <DoneIcon />
+//           </IconButton>
+//         </CardContent>
+//       </Card>
+//     );
+//   } else if (viewMode === "detailed") {
+//     return (
+//       <RequestBox>
+//         <IconButton
+//             aria-label="delete"
+//             style={{ float: "right" }}
+//             onClick={() => onDelete(form.username)} // You should pass a unique identifier like form.id for each booking
+//           >
+//             <DeleteIcon />
+//           </IconButton>
+//         <Typography variant="h6" style={{ color: "#000" }}>
+//           Your Request Details:
+//         </Typography>
+//         <Typography style={{ color: "#000" }}>From: {form.from}</Typography>
+//         <Typography style={{ color: "#000" }}>To: {form.to}</Typography>
+//         <Typography style={{ color: "#000" }}>
+//           Leaving Time: {form.leaveTime}
+//         </Typography>
+//         <Typography style={{ color: "#000" }}>
+//           CreatedBY: {form.username}
+//         </Typography>
+//         <Typography style={{ color: "#000" }}>Status: {form.status === 1 ? "Active" : form.status}</Typography>
+//       </RequestBox>
+//     );
+//   }
+// };
+
+// export default RequestCard;
 import React from "react";
 import {
   Card,
   CardContent,
   Typography,
   IconButton,
+  Button,
   Box,
   styled,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import DoneIcon from '@mui/icons-material/Done';
-import {deletUserBooking} from "../../API/fetchApi.js"
+import { deletUserBooking } from "../../API/fetchApi.js";
 
 const RequestBox = styled(Box)`
   border: 1px solid #000;
@@ -55,36 +135,34 @@ const RequestBox = styled(Box)`
   margin-top: 20px;
   width: 90%;
   text-align: center;
-  background-color: #fff; 
+  background-color: #fff;
 `;
 
 const RequestCard = ({ form, viewMode }) => {
+  // Deleting the request
+  const onDelete = async (username) => {
+    const res = await deletUserBooking(username);
+  };
 
-    //deleting the request
-
-    const onDelete = async(username)=>{
-         const res = await deletUserBooking(username)
-        
-    }
-   const onAccept = ()=>{}
-
-
-
+  const onBook = () => {
+    // Implement your booking logic here
+  };
 
   if (viewMode === "compact") {
     return (
       <Card style={{ marginBottom: "10px" }}>
         <CardContent>
           <Typography variant="body1" style={{ display: "inline-block" }}>
-            {form.from} --To--  {form.to}  -- Created By -- {form.username}
+            {form.from} --To-- {form.to} -- Created By -- {form.username}
           </Typography>
-          <IconButton
-            aria-label="delete"
+          <Button
+            variant="contained"
+            color="primary"
             style={{ float: "right" }}
-            onClick={() => onAccept(form.id)} // You should pass a unique identifier like form.id for each booking
+            onClick={onBook}
           >
-            <DoneIcon />
-          </IconButton>
+            Book
+          </Button>
         </CardContent>
       </Card>
     );
@@ -92,14 +170,14 @@ const RequestCard = ({ form, viewMode }) => {
     return (
       <RequestBox>
         <IconButton
-            aria-label="delete"
-            style={{ float: "right" }}
-            onClick={() => onDelete(form.username)} // You should pass a unique identifier like form.id for each booking
-          >
-            <DeleteIcon />
-          </IconButton>
+          aria-label="delete"
+          style={{ float: "right" }}
+          onClick={() => onDelete(form.username)}
+        >
+          <DeleteIcon />
+        </IconButton>
         <Typography variant="h6" style={{ color: "#000" }}>
-          Ride Request Details:
+          Your Request Details:
         </Typography>
         <Typography style={{ color: "#000" }}>From: {form.from}</Typography>
         <Typography style={{ color: "#000" }}>To: {form.to}</Typography>
@@ -107,9 +185,19 @@ const RequestCard = ({ form, viewMode }) => {
           Leaving Time: {form.leaveTime}
         </Typography>
         <Typography style={{ color: "#000" }}>
-          CreatedBY: {form.username}
+          Created BY: {form.username}
         </Typography>
-        <Typography style={{ color: "#000" }}>Status: {form.status === 1 ? "Active" : form.status}</Typography>
+        <Typography style={{ color: "#000" }}>
+          Status: {form.status === 1 ? "Active" : form.status}
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginTop: "10px" }}
+          onClick={onBook}
+        >
+          Book
+        </Button>
       </RequestBox>
     );
   }
